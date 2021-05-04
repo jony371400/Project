151f208b56fa024df8b44a2b2ba92ed528a355ab
jony <jony371400@gmail.com>
2021-05-04 09:46:21 +0800

    05040946

---
+++ MG100/MG100/LogData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG100
+{
+    public class LogData
+    {
+        string Road = "";
+        public List<string> AgvMsg = new List<string>();
+
+        public LogData()
+        {
+            string Env = Environment.CurrentDirectory;
+            string Time = DateTime.Now.ToString("MMddHHmm");
+            Road = Env + "\\Log\\" + Time + ".txt";
+        }
+
+        public void SaveLog()
+        {
+            StreamWriter sw = new StreamWriter(Road);
+            foreach (string msg in AgvMsg)
+                sw.WriteLine(msg);
+            sw.Close();
+        }
+    }
+}
